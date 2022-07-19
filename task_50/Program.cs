04699961fcf49a0cc98fcd5607d25f4b3ba08808
Spec-75 <Spec_75@mail.ru>
2022-07-19 17:46:10 +0300

    add task_50

---
+++ task_50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Write("Введите номер строки массива от 1 до 4, интересующего элемента: ");
+int numberRow = int.Parse(Console.ReadLine()!);
+int rows = 4;//
+Console.Write("Введите номер столбца массива от 1 до 4, интересующего элемента: ");
+int numberColumn = int.Parse(Console.ReadLine()!);
+int BuferI = int.MinValue;
+int BuferJ = int.MinValue;
+
+int columns = 4;//Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+Console.WriteLine("matrix:");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    if (i == numberRow - 1) BuferI = i;
+    // else Console.WriteLine($"Элемент не найден");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(-10, 11);
+        Console.Write(matrix[i, j] + "\t");
+        if (j == numberColumn - 1) BuferJ = j;
+        // else Console.WriteLine($"Элемент не найден");
+    }
+    Console.WriteLine();
+}
+if (BuferI == int.MinValue || BuferJ == int.MinValue) 
+{
+    Console.WriteLine($"Элемент с индексами({numberRow}, {numberColumn}) не найден.");
+}
+else 
+{
+    Console.WriteLine($"Элемент с индексами({numberRow}, {numberColumn}) равен {matrix[BuferI, BuferJ]}");
+}
