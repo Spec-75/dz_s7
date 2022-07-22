29c3308d6f4e870e3d1572a755958f8e0b9a4511
Spec-75 <Spec_75@mail.ru>
2022-07-22 07:34:55 +0300

    add task_52

--- task_47/Program.cs
+++ task_47/Program.cs
@@ -16,7 +16,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i,j] = new Random().NextDouble()*100;
+        matrix[i,j] = Math.Round(new Random().NextDouble()*100, 2);
         Console.Write(matrix[i,j] + "\t");
     }
     Console.WriteLine();
---
+++ task_52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("enter the number of rows:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of columns:");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+double[] sum = new double[columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = new Random().Next(100);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < columns; i++)
+{
+    for (int j = 0; j < rows; j++)
+    {
+        sum[i] += matrix[j, i];
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < columns; i++)
+{
+    Console.Write((sum[i] / matrix.GetLength(0)) + "\t");
+}
